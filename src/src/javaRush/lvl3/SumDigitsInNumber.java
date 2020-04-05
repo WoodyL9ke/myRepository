f96c8916fd0157071eb1596466a2e09cf7bb3b8b
WoodyL9ke <WoodyL9ke1337@gmail.com>
2020-04-05 18:29:31 +0700

    dsds

---
+++ src/src/javaRush/lvl3/SumDigitsInNumber.java
@@ -0,0 +1,15 @@
+package javaRush.lvl3;
+
+public class SumDigitsInNumber {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(557));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+        int b = number / 100;
+        int a = number  %100 / 10;
+        int c = number % 100 % 10;
+        return b + a + c;
+        //напишите тут ваш код
+    }
+}
